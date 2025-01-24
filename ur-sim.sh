d6db24d0b2ca4e02af4b1f2be855caa09b54e98e
Torger Bocianowski <torgerboc@gmail.com>
2025-01-24 23:13:49 +0100

    start ur sim script

---
+++ ur-sim.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+docker run --rm -it\
+    -p 5900:5900\
+    -p 6080:6080\
+    -p 29999-30004:29999-30004\
+    --platform linux/amd64\
+    --privileged\
+    -e ROBOT_MODEL=UR20\
+    --name ursim\
+    universalrobots/ursim_e-series\
+    "control_log"
